3a847c431561375ec776f66e902c7632f68e47db
Tyler Leite <tyler@ottoq.com>
2019-03-08 11:08:15 -0800

    server endpoint to serve tokens

--- server.py
+++ server.py
@@ -11,22 +11,49 @@ class RESTHandler(http.server.BaseHTTPRequestHandler):
     binary = self.rfile.read(length)
     return json.loads(binary.decode())
 
-  def do_POST(self):
-    body = self.get_body()
-
-    s = Spell()
-    print(body["words"].split(' '))
-    s.compile(body["words"].split(' '))
-    res = json.dumps(s.json_data(body["words"].split(' ')))
-
-    self.send_response(200)
+  def default_headers(self):
     self.send_header("Content-Type", "application/json")
     self.send_header("Access-Control-Allow-Origin", "*")
     self.send_header("Access-Control-Expose-Headers", "Access-Control-Allow-Origin")
     self.send_header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+
+  def do_POST(self):
+    print(self.path)
+
+    res = None
+    if self.path == '/compile':
+      res = self.do_POST_compile()
+
+    self.send_response(200)
+    self.default_headers()
     self.end_headers()
     self.wfile.write(bytes(res, "utf-8"))
 
+  def do_POST_compile(self):
+    body = self.get_body()
+
+    s = Spell()
+    s.compile(body["words"].split(' '))
+    res = json.dumps(s.json_data(body["words"].split(' ')))
+    return res
+
+  def do_GET(self):
+    print(self.path)
+    res = None
+    if self.path == '/tokens':
+      res = self.do_GET_tokens()
+
+    self.send_response(200)
+    self.default_headers()
+    self.end_headers()
+    self.wfile.write(bytes(res, "utf-8"))
+
+  def do_GET_tokens(self):
+    out = ''
+    with open('tokens.json', 'r') as file:
+      for line in file:
+        out += line + '\n'
+    return out
 
 PORT = 20394
 Handler = RESTHandler
